0e33d8f1bd1856ca144ffac8275c833e51754371
sun-ji054 <seonji04203645@gmail.com>
2025-12-28 14:12:40 +0900

    FEAT: tailwind 적용 완료

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,8 +2,23 @@ import React from "react";
 
 export default function Navigation() {
   return (
-    <div className="flex top-0 bg-blue-500">
-      <p>안녕</p>
-    </div>
+    <header className="sticky top-0 z-50 w-full h-16 bg-white/80 backdrop-blur-md border-b border-indigo-200 shadow-sm">
+      <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center h-16">
+          <div className="flex items-center gap-2">
+            <p>안녕</p>
+            <p>hi</p>
+          </div>
+          <nav className="flex">
+            <ul className="hidden md:flex space-x-8 text-sm font-medium text-gray-600">
+              <li>홈</li>
+              <li>홈</li>
+              <li>홈</li>
+              <li>홈</li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
   );
 }
