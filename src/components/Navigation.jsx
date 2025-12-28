b97cb6de34c8dfdf5120451f0a806c56d5237251
sun-ji054 <seonji04203645@gmail.com>
2025-12-28 10:07:21 +0900

    FEAT: tailwind 세팅 완료

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navigation() {
+  return (
+    <div className="flex top-0 bg-blue-500">
+      <p>안녕</p>
+    </div>
+  );
+}
--- src/pages/XGBoost.tsx
+++ src/pages/XGBoost.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import Navigation from "../components/Navigation";
 
 export default function XCBoost() {
   return (
     <>
-      <p>안녕</p>
+      <Navigation />
     </>
   );
 }
